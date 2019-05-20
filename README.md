205a660e92005896609657daf3e44b0c8ca0c027
wilferraciolli <wiliam334@hotmail.com>
2019-05-20 16:46:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wq7rcf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wq7rcf)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HeaderComponent, ContentComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
